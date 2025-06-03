334eb02cde2d7f8ee63a09bc36fe38b684122cc1
Yurii <98182161+YuraVitoshko@users.noreply.github.com>
2025-06-03 15:12:44 +0300

    Add files via upload

--- js/dev/index.js
+++ js/dev/index.js
@@ -152,6 +152,9 @@ let bodyLock = (delay = 500) => {
     }, delay);
   }
 };
+function uniqArray(array) {
+  return array.filter((item, index, self) => self.indexOf(item) === index);
+}
 function dataMediaQueries(array, dataSetValue) {
   const media = Array.from(array).filter((item) => item.dataset[dataSetValue]).map((item) => {
     const [value, type = "max"] = item.dataset[dataSetValue].split(",");
@@ -5033,6 +5036,124 @@ class DynamicAdapt {
 if (document.querySelector("[data-fls-dynamic]")) {
   window.addEventListener("load", () => new DynamicAdapt());
 }
+class ScrollWatcher {
+  constructor(props) {
+    let defaultConfig = {
+      logging: true
+    };
+    this.config = Object.assign(defaultConfig, props);
+    this.observer;
+    !document.documentElement.classList.contains("watcher") ? this.scrollWatcherRun() : null;
+  }
+  // Оновлюємо конструктор
+  scrollWatcherUpdate() {
+    this.scrollWatcherRun();
+  }
+  // Запускаємо конструктор
+  scrollWatcherRun() {
+    document.documentElement.classList.add("watcher");
+    this.scrollWatcherConstructor(document.querySelectorAll("[data-fls-watcher]"));
+  }
+  // Конструктор спостерігачів
+  scrollWatcherConstructor(items) {
+    if (items.length) {
+      let uniqParams = uniqArray(Array.from(items).map(function(item) {
+        if (item.dataset.flsWatcher === "navigator" && !item.dataset.flsWatcherThreshold) {
+          let valueOfThreshold;
+          if (item.clientHeight > 2) {
+            valueOfThreshold = window.innerHeight / 2 / (item.clientHeight - 1);
+            if (valueOfThreshold > 1) {
+              valueOfThreshold = 1;
+            }
+          } else {
+            valueOfThreshold = 1;
+          }
+          item.setAttribute(
+            "data-fls-watcher-threshold",
+            valueOfThreshold.toFixed(2)
+          );
+        }
+        return `${item.dataset.flsWatcherRoot ? item.dataset.flsWatcherRoot : null}|${item.dataset.flsWatcherMargin ? item.dataset.flsWatcherMargin : "0px"}|${item.dataset.flsWatcherThreshold ? item.dataset.flsWatcherThreshold : 0}`;
+      }));
+      uniqParams.forEach((uniqParam) => {
+        let uniqParamArray = uniqParam.split("|");
+        let paramsWatch = {
+          root: uniqParamArray[0],
+          margin: uniqParamArray[1],
+          threshold: uniqParamArray[2]
+        };
+        let groupItems = Array.from(items).filter(function(item) {
+          let watchRoot = item.dataset.flsWatcherRoot ? item.dataset.flsWatcherRoot : null;
+          let watchMargin = item.dataset.flsWatcherMargin ? item.dataset.flsWatcherMargin : "0px";
+          let watchThreshold = item.dataset.flsWatcherThreshold ? item.dataset.flsWatcherThreshold : 0;
+          if (String(watchRoot) === paramsWatch.root && String(watchMargin) === paramsWatch.margin && String(watchThreshold) === paramsWatch.threshold) {
+            return item;
+          }
+        });
+        let configWatcher = this.getScrollWatcherConfig(paramsWatch);
+        this.scrollWatcherInit(groupItems, configWatcher);
+      });
+    }
+  }
+  // Функція створення налаштувань
+  getScrollWatcherConfig(paramsWatch) {
+    let configWatcher = {};
+    if (document.querySelector(paramsWatch.root)) {
+      configWatcher.root = document.querySelector(paramsWatch.root);
+    } else if (paramsWatch.root !== "null") ;
+    configWatcher.rootMargin = paramsWatch.margin;
+    if (paramsWatch.margin.indexOf("px") < 0 && paramsWatch.margin.indexOf("%") < 0) {
+      return;
+    }
+    if (paramsWatch.threshold === "prx") {
+      paramsWatch.threshold = [];
+      for (let i = 0; i <= 1; i += 5e-3) {
+        paramsWatch.threshold.push(i);
+      }
+    } else {
+      paramsWatch.threshold = paramsWatch.threshold.split(",");
+    }
+    configWatcher.threshold = paramsWatch.threshold;
+    return configWatcher;
+  }
+  // Функція створення нового спостерігача зі своїми налаштуваннями
+  scrollWatcherCreate(configWatcher) {
+    this.observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach((entry) => {
+        this.scrollWatcherCallback(entry, observer);
+      });
+    }, configWatcher);
+  }
+  // Функція ініціалізації спостерігача зі своїми налаштуваннями
+  scrollWatcherInit(items, configWatcher) {
+    this.scrollWatcherCreate(configWatcher);
+    items.forEach((item) => this.observer.observe(item));
+  }
+  // Функція обробки базових дій точок спрацьовування
+  scrollWatcherIntersecting(entry, targetElement) {
+    if (entry.isIntersecting) {
+      !targetElement.classList.contains("--watcher-view") ? targetElement.classList.add("--watcher-view") : null;
+    } else {
+      targetElement.classList.contains("--watcher-view") ? targetElement.classList.remove("--watcher-view") : null;
+    }
+  }
+  // Функція відключення стеження за об'єктом
+  scrollWatcherOff(targetElement, observer) {
+    observer.unobserve(targetElement);
+  }
+  // Функція обробки спостереження
+  scrollWatcherCallback(entry, observer) {
+    const targetElement = entry.target;
+    this.scrollWatcherIntersecting(entry, targetElement);
+    targetElement.hasAttribute("data-fls-watcher-once") && entry.isIntersecting ? this.scrollWatcherOff(targetElement, observer) : null;
+    document.dispatchEvent(new CustomEvent("watcherCallback", {
+      detail: {
+        entry
+      }
+    }));
+  }
+}
+document.querySelector("[data-fls-watcher]") ? window.addEventListener("load", () => new ScrollWatcher({})) : null;
 function pageNavigation() {
   document.addEventListener("click", pageNavigationAction);
   document.addEventListener("watcherCallback", pageNavigationAction);
@@ -8560,12 +8681,28 @@ document.addEventListener("DOMContentLoaded", function() {
   const pauseButton = document.getElementById("pause");
   const muteButton = document.getElementById("mute");
   const fullscreenButton = document.getElementById("fullscreen");
+  const videoContainer = document.querySelector(".media-hero__video");
+  let hideControlsTimeout;
+  const isMobile = /iPhone|iPad|iPod|Android|Mobile|Windows Phone/i.test(navigator.userAgent);
   muteButton.innerHTML = '<img src="assets/img/icons/sound-off.svg" alt="Mute">';
   videoPlayer.muted = true;
-  videoPlayer.pause();
-  playButton.style.display = "block";
-  pauseButton.style.display = "none";
-  navvControls.classList.remove("hidden");
+  if (!isMobile) {
+    videoPlayer.play().then(() => {
+      playButton.style.display = "none";
+      pauseButton.style.display = "block";
+      navvControls.classList.add("hidden");
+    }).catch((err) => {
+      console.warn("Автовідтворення заблоковане:", err);
+      playButton.style.display = "block";
+      pauseButton.style.display = "none";
+      navvControls.classList.remove("hidden");
+    });
+  } else {
+    videoPlayer.pause();
+    playButton.style.display = "block";
+    pauseButton.style.display = "none";
+    navvControls.classList.remove("hidden");
+  }
   videoPlayer.addEventListener("play", function() {
     playButton.style.display = "none";
     pauseButton.style.display = "block";
@@ -8620,25 +8757,21 @@ document.addEventListener("DOMContentLoaded", function() {
       }
     }
   });
-  const videoContainer = document.querySelector(".media-hero__video");
-  const navControls = document.querySelector(".nav-video");
-  let hideControlsTimeout;
   function showControls() {
-    navControls.classList.remove("hidden");
+    navvControls.classList.remove("hidden");
     clearTimeout(hideControlsTimeout);
     hideControlsTimeout = setTimeout(() => {
-      navControls.classList.add("hidden");
+      navvControls.classList.add("hidden");
     }, 2e3);
   }
   videoContainer.addEventListener("mouseenter", () => {
-    navControls.classList.remove("hidden");
-    clearTimeout(hideControlsTimeout);
+    showControls();
   });
   videoContainer.addEventListener("mousemove", () => {
     showControls();
   });
   videoContainer.addEventListener("mouseleave", () => {
-    navControls.classList.add("hidden");
+    navvControls.classList.add("hidden");
     clearTimeout(hideControlsTimeout);
   });
   let lastTapTime = 0;
@@ -8647,10 +8780,10 @@ document.addEventListener("DOMContentLoaded", function() {
     const tapInterval = currentTime - lastTapTime;
     if (tapInterval < 300) return;
     lastTapTime = currentTime;
-    if (navControls.classList.contains("hidden")) {
+    if (navvControls.classList.contains("hidden")) {
       showControls();
     } else {
-      navControls.classList.add("hidden");
+      navvControls.classList.add("hidden");
       clearTimeout(hideControlsTimeout);
     }
   });
